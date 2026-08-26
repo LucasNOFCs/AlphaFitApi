6cc46987a2f307e1b6fcf6d787859e0a3d28c611
LucasNOFCs <lucasnoobreoficial@gmail.com>
2026-08-26 16:11:16 -0300

    models generated, with roles

--- app/Models/Member.php
+++ app/Models/Member.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Member extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'number',
+        'email',
+        'plan_id',
+    ];
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+
+        ];
+    }
 }
--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -2,9 +2,42 @@
 
 namespace App\Models;
 
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'member_id',
+        'plan_id',
+        'amount',
+        'payment_date',
+        'status',
+        ];
+        
+    public function member()
+    {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function plan()
+    {
+        return $this->belongsTo(Plan::class);
+    }
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'amount' => 'decimal:2',
+            'payment_date' => 'datetime',
+        ];
+    }
 }
