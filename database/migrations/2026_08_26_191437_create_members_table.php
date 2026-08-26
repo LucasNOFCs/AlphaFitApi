285fff2f6295105b232fbe513305dd1f729b4d4b
LucasNOFCs <lucasnoobreoficial@gmail.com>
2026-08-26 16:31:49 -0300

    migrations prepared

--- database/migrations/2026_08_26_191437_create_members_table.php
+++ database/migrations/2026_08_26_191437_create_members_table.php
@@ -11,11 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('members', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('number');
             $table->string('email')->unique();
-            $table->string('password');
+
+            $table->foreignId('plan_id')
+                ->constrained('plans')
+                ->onDelete('cascade');
+
             $table->timestamps();
         });
     }
@@ -25,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('members');
     }
 };
